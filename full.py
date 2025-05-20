17c9bf063119e000c40455fa3f2f88856e3d0280
ben28wettstein <wettsteinben@gmail.com>
2025-05-19 23:07:25 -0400

    explainability for full

--- full.py
+++ full.py
@@ -15,10 +15,12 @@ cyborg = CybORG(scenario_generator=sg, seed=1234)
 
 # Record actions of red_agent_0
 red_agent_0_actions = []
-for i in range(20):
-    cyborg.step()
+for i in range(15):
+    results = cyborg.step(agent='red_agent_0')
+    #print(results.observation) prints red agents observation
     step_actions = cyborg.environment_controller.action
     red_agent_0_actions.append(step_actions['red_agent_0'])
+    
 
 # Print red_agent_0's actions
 #pprint(red_agent_0_actions)
